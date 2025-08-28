9364ffe4d959e80c21d14daea8aba925bd2e411b
ryannnchang <ryanc8764@gmail.com>
2025-08-28 18:24:55 -0400

    updated map component

--- src/pages/game.jsx
+++ src/pages/game.jsx
@@ -1,19 +1,12 @@
 import * as React from 'react';
 import {Map} from '@vis.gl/react-maplibre';
 import 'maplibre-gl/dist/maplibre-gl.css'; // See notes bel
+import Mapview from '../components/MapView';
 
 export function Game() {
   return (
-    <div style={{ width: '100vw', height: '100vh' }}>
-      <Map
-        initialViewState={{
-        longitude: -79.919152,
-        latitude:  43.261103,
-        zoom: 16
-        }}
-        style={{width: '100%', height: '100%'}}
-        mapStyle="https://tiles.openfreemap.org/styles/liberty"
-      />
-    </div>
+    <>
+    <Mapview />
+    </>
   );
 }
